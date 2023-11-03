c6e8ee98b706c1f42c4c477d61db803ed440a98b
Birkity <85987355+Birkity@users.noreply.github.com>
2023-11-03 15:21:03 +0300

    Async/await function

--- currency-converter.js
+++ currency-converter.js
@@ -2,14 +2,14 @@
 //https://restcountries.eu/rest/v2/currency/
 const axios = require('axios');
 
-const getExchangeRate = (fromCurrency, toCurrency) => {
-    axios.get('http://apilayer.net/api/live?access_key=884773bb76e704548d026928debd1ab3&format=1').then((response) => {
-         const rate = response.data.quotes;
-         
-         console.log(rate);
-
-    });
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+    const response =  await axios.get('http://apilayer.net/api/live?access_key=884773bb76e704548d026928debd1ab3&format=1');
+    const rate = response.data.quotes;
+    const euro = 1/ rate[fromCurrency];
+    const exchangeRate = euro * rate[toCurrency];
+    
+    console.log(exchangeRate);
   
 }
 
-getExchangeRate('USD', 'EUR');
+getExchangeRate('USDRWF', 'USDSAR');
